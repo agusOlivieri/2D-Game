c8ecdd229ff7d431d0c4175c67fce7f728e5d3be
agus <agustinolivieri30@gmail.com>
2025-12-07 22:18:24 -0300

    initial configuration

---
+++ src/main/GamePanel.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.blue);
+        this.setDoubleBuffered(true);
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("My 2D Game in Java!");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
