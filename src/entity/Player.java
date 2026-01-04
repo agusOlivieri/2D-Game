18271dbd42690a962703907e0f80a1674a2ac082
agus <agustinolivieri30@gmail.com>
2026-01-04 17:16:36 -0300

    changed player image

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -39,10 +39,14 @@ public class Player extends Entity{
     public void getPlayerImage() {
         try {
 
-            up = ImageIO.read(getClass().getResourceAsStream("/player/github-octopuss-2.png"));
-            down = ImageIO.read(getClass().getResourceAsStream("/player/github-octopuss-2.png"));
-            right = ImageIO.read(getClass().getResourceAsStream("/player/github-octopuss-2.png"));
-            left = ImageIO.read(getClass().getResourceAsStream("/player/github-octopuss-2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -83,6 +87,16 @@ public class Player extends Entity{
                         break;
                 }
             }
+
+            spriteCounter++;
+            if (spriteCounter > 12) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
@@ -92,16 +106,36 @@ public class Player extends Entity{
 
         switch (direction) {
             case "up":
-                image = up;
+                if(spriteNum == 1) {
+                    image = up1;
+                }
+                if(spriteNum == 2) {
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down;
+                if(spriteNum == 1) {
+                    image = down1;
+                }
+                if(spriteNum == 2) {
+                    image = down2;
+                }
                 break;
             case "right":
-                image = right;
+                if(spriteNum == 1) {
+                    image = right1;
+                }
+                if(spriteNum == 2) {
+                    image = right2;
+                }
                 break;
             case "left":
-                image = left;
+                if(spriteNum == 1) {
+                    image = left1;
+                }
+                if(spriteNum == 2) {
+                    image = left2;
+                }
                 break;
         }
 
