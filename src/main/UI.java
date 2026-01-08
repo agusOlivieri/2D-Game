91bb95b1ebbae5bf59e856d334fbe979ead77967
agus <agustinolivieri30@gmail.com>
2026-01-08 14:55:30 -0300

    display player life

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,6 +1,8 @@
 package main;
 
+import object.OBJ_Heart;
 import object.OBJ_Key;
+import object.SuperObject;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -9,7 +11,8 @@ public class UI {
 
     GamePanel gamePanel;
     Font arial_40;
-    BufferedImage keyImage;
+    BufferedImage keyImage, heart_full, heart_half, heart_blank;
+    Graphics2D g2;
 
     public UI(GamePanel gp) {
         this.gamePanel = gp;
@@ -17,13 +20,55 @@ public class UI {
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         OBJ_Key key = new OBJ_Key(gamePanel);
         keyImage = key.image;
+
+        // CREATE HUD OBJECT
+        SuperObject heart = new OBJ_Heart(gp);
+        heart_full = heart.image;
+        heart_half = heart.image2;
+        heart_blank = heart.image3;
     }
 
     public void draw(Graphics2D g2) {
+
+        this.g2 = g2;
+
         g2.setFont(arial_40);
         g2.setColor(Color.white);
-        g2.drawImage(keyImage, gamePanel.tileSize/2, gamePanel.tileSize/2, gamePanel.tileSize, gamePanel.tileSize, null);
-        g2.drawString("X = " + gamePanel.player.hasKey, 74, 65);
+        g2.drawImage(keyImage, gamePanel.tileSize/2, gamePanel.tileSize + 25, gamePanel.tileSize, gamePanel.tileSize, null);
+        g2.drawString("X = " + gamePanel.player.hasKey, 74, gamePanel.tileSize * 2 + 25);
+
+        drawPlayerLife();
+    }
+
+    public void drawPlayerLife() {
+
+        int x = gamePanel.tileSize/2;
+        int y = gamePanel.tileSize/2;
+        int i = 0;
+
+        // DRAW MAX LIFE
+        while (i < gamePanel.player.maxLife/2) {
+            g2.drawImage(heart_blank, x, y, null);
+            i++;
+            x += gamePanel.tileSize;
+        }
+
+        // RESET
+        x = gamePanel.tileSize/2;
+        y = gamePanel.tileSize/2;
+        i = 0;
+
+        // DRAW CURRENT LIFE
+        while (i < gamePanel.player.life) {
+            g2.drawImage(heart_half, x, y, null);
+            i++;
+
+            if (i < gamePanel.player.life) {
+                g2.drawImage(heart_full, x, y, null);
+            }
+            i++;
+            x += gamePanel.tileSize;
+        }
     }
 
 }
